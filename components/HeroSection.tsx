701a9875f7f4972fb0c602952e7b77280f6cd020
Cocochoiii <choi.coco@northeastern.edu>
2026-01-02 01:32:10 -0500

    fix number

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -251,7 +251,7 @@ export default function HeroSection() {
                                         className="font-semibold text-neutral-900 border-b-2 border-primary-700 inline-block"
                                         whileHover={{ scale: 1.1 }}
                                     >
-                                        16 dogs
+                                        17 dogs
                                     </motion.span>{' '}
                                 </motion.p>
 
